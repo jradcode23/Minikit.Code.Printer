8ebed24fd735607e818851816d3a87da9380cc32
jrad <215300829+jradcode23@users.noreply.github.com>
2026-08-31 21:52:28 -0700

    Setup the Lego Pirates Minikit Printer

--- BuildLinked.ps1
+++
@@ -1,9 +0,0 @@
-# Set Working Directory
-Split-Path $MyInvocation.MyCommand.Path | Push-Location
-[Environment]::CurrentDirectory = $PWD
-
-Remove-Item "$env:RELOADEDIIMODS/LMSH.Minikit.Codes/*" -Force -Recurse
-dotnet publish "./LMSH.Minikit.Codes.csproj" -c Release -o "$env:RELOADEDIIMODS/LMSH.Minikit.Codes" /p:OutputPath="./bin/Release" /p:ReloadedILLink="true"
-
-# Restore Working Directory
-Pop-Location
